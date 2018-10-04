0f72770aafa9bf7ad0caa6247159d49312faf0aa
francisco hernandez <fran.hernandez@desarrollonegocios.com>
2018-10-04 16:06:23 -0600

    Edit

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -10,15 +10,16 @@
 <title>eContacts</title>
 <!-- head -->
 <!-- Styling -->
-<link href="../../assets/css/css" rel="stylesheet">
-<link href="../../assets/css/all.min.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>assets/css/css" rel="stylesheet">
+<link href="<?php echo base_url()?>assets/css/all.min.css" rel="stylesheet">
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
-<script src="../assets/js/scripts.min.js"></script>        
+<?php echo base_url(); ?>
+<script src="<?php echo base_url(); ?>assets/css/scripts.min.js"></script>        
 <script type="text/javascript">
 	jQuery(document).ready(function(){
 
